ad57b3159b028c8737990b9e3cec9a0b326e23be
Suyashtnt <suyashtnt@gmail.com>
2025-01-26 22:25:46 +0200

    feat: redo compression and transpilation
    Turns out, lightningcss actually hurts bundle size
    here. I think it's because it's trying to transpile
    for older browsers.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 It's a file compressor in a QR code. Shrimple as that
 
 ![Screenshot of CompressQR](./screenshot.png)
+![QR Code of CompressQR](./qrcode.png)
 
 # Compiling and running
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { compression } from "vite-plugin-compression2";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import { createHtmlPlugin } from "vite-plugin-html";
 import zlib from "node:zlib";
 
 // https://vite.dev/config/
@@ -9,6 +10,9 @@ export default defineConfig({
     viteSingleFile({
       useRecommendedBuildConfig: true,
       removeViteModuleLoader: true,
+    }),
+    createHtmlPlugin({
+      minify: true,
     }),
     compression({
       algorithm: "brotliCompress",
@@ -19,27 +23,4 @@ export default defineConfig({
       },
     }),
   ],
-  build: {
-    cssMinify: "lightningcss",
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        keep_fargs: false,
-        unsafe_undefined: true,
-        unsafe_math: true,
-        unsafe_Function: true,
-        unsafe_arrows: true,
-        unsafe_methods: true,
-        pure_new: true,
-        passes: 5,
-      },
-      mangle: {
-        toplevel: true,
-        module: true,
-      },
-      ecma: 2020,
-    },
-  },
 });
